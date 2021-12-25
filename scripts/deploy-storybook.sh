ec7164af3b15f193e055b5b4bddd80ecc9cd9c0f
Marc Donald <dev@marcdonald.com>
2021-12-25 01:18:41 +0000

    Change Storybook deployment branch

--- scripts/deploy-storybook.sh
+++ scripts/deploy-storybook.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
 yarn run build-storybook
-storybook-to-ghpages --existing-output-dir=.storybook-public
+storybook-to-ghpages --existing-output-dir=.storybook-public --branch=storybook-deploy
